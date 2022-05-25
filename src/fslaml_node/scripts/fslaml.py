51cd5c53476b4acafbe600cf6a6b614f823b90c8
Ravi Regalo <raviregalo@gmail.com>
2022-05-25 12:34:43 -0700

    com rosbag

--- src/fslaml_node/scripts/fslaml.py
+++ src/fslaml_node/scripts/fslaml.py
@@ -23,7 +23,8 @@ try:
 except BaseException as error:
     pass
 
-M_PARTICLES = 1000
+M_PARTICLES = 100
+ROOM_SIZE = 4
 
 # Defines the shape of the particles
 class Particle():
@@ -40,8 +41,8 @@ class Particle_set():
         pass
     
     def gen_random(self):
-        #p = uniform((-10, -10, -pi/2),(10, 10, pi/2),3)
-        p = uniform((-0.5, -0.5, -0.01),(0.5, 0.5, 0.01),3)
+        #p = uniform((-ROOM_SIZE, -ROOM_SIZE, -pi/2),(ROOM_SIZE, ROOM_SIZE, pi/2),3)
+        p = np.zeros(3)     # assume boot satarting location as inertial origin
         ldmrks = []
         return Particle(p[0], p[1], p[2], ldmrks, 1)
 
@@ -57,14 +58,13 @@ class ParticleFilter():
         for i in range(M_PARTICLES):
             self.Xt.append(p_set.gen_random())
             self.Xt[i].w = 1/M_PARTICLES
-        self.x = 0
-        self.y = 0
-        self.teta = 0  
-    
+
+        #TODO: this is an image representaation, some kind of plot would be better, força malucos
     def show_particle_state(self): #TODO add orientation to this representation so it looks nicer
         img = np.zeros((700,700,3), dtype=np.uint8)
         for p in self.Xt:
-            cv2.circle(img, (round(int(p.x)*35) + 350, round(int(p.y)*35) + 350), 3, (255, 0, 0), cv2.FILLED)
+            center = (int(p.x*210) + 350, int(p.y*70) +350)
+            cv2.circle(img, center, 4, (0, 255, 0), cv2.FILLED)
         self.img_pub.publish(self.bridge.cv2_to_imgmsg(img))
 
 
@@ -78,16 +78,14 @@ class ParticleFilter():
             self.prev_t = curr_t
             delta_teta = odom.twist.twist.angular.z * timing
             delta_x = odom.twist.twist.linear.x * timing
-            self.teta += delta_teta
-            self.x += delta_x*cos(self.teta)
-            self.y += delta_x*sin(self.teta)
-            self.pub.publish(str(self.x) + "\n" + str(self.y) + "\n" + str(self.teta) + "\n")
-            # for i in range(len(self.Xt)):
-            #     self.Xt[i].teta += delta_teta
-            #     self.Xt[i].x += delta_x*cos(self.Xt[i].teta)
-            #     self.Xt[i].y += delta_x*sin(self.Xt[i].teta)
-            # #calculate weights    
-            # self.show_particle_state()
+            for i in range(len(self.Xt)):
+                self.Xt[i].teta += delta_teta + np.random.normal(0, abs(delta_teta/(2*1.645))) # delta
+                self.Xt[i].x += (delta_x + np.random.normal(0, abs(delta_x/(5*1.645))))*cos(self.Xt[i].teta)
+                self.Xt[i].y += (delta_x + np.random.normal(0, abs(delta_x/(5*1.645))))*sin(self.Xt[i].teta)
+            
+
+            #calculate weights    
+            self.show_particle_state()
 
 
 
@@ -99,9 +97,9 @@ def main(args):
     #scan_sub = Subscriber('lidar', PointCloud2)
     info_pub = rospy.Publisher('info', String, queue_size=2)
     image_pub = rospy.Publisher('particles', Image, queue_size=2)
-    # map_pub = rospy.Publisher('detected_cloud', PointCloud2, queue_size=2)
 
     pf = ParticleFilter(info_pub, image_pub)
+    pf.show_particle_state()
 
     ats = ApproximateTimeSynchronizer([odom_sub, imu_sub], queue_size=10, slop=0.3, allow_headerless=False)
     ats.registerCallback(pf.callback)
